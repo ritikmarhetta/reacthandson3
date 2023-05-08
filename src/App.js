84df5e3e6c517055d3b8aa7943bd681410551e2a
ritikmarhetta <118370440+ritikmarhetta@users.noreply.github.com>
2023-05-08 20:59:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './COMPONENTS/style.css';
+import Form from './COMPONENTS/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
